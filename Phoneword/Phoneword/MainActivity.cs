45470018a2af75277b0760b99444b8c30eeff26a
Matthew Chin <chin.matthew1212@gmail.com>
2017-11-20 20:36:43 -0500

    Finished Phoneword
    Added functionality to text boxes and button for translating phone words.

--- Phoneword/Phoneword/MainActivity.cs
+++ Phoneword/Phoneword/MainActivity.cs
@@ -1,18 +1,42 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
 using Android.Widget;
 using Android.OS;
 
 namespace Phoneword
 {
-    [Activity(Label = "Phoneword", MainLauncher = true)]
+    [Activity(Label = "Phone Word", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(savedInstanceState);
+            base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            // Get UI Controls from layout.
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+
+            // Add functionality to translate button.
+            translateButton.Click += (sender, e) =>
+            {
+                // Convert input received in field to number.
+                string translatedNumber = Core.PhoneTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoneWord.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoneWord.Text = translatedNumber;
+                }
+            };
+
+
         }
     }
 }
---
+++ Phoneword/Phoneword/PhoneTranslator.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+using System;
+
+namespace Core
+{
+    public static class PhoneTranslator
+    {
+        // Convert input from user to numbers.
+        public static String ToNumber(string raw)
+        {
+            if (string.IsNullOrEmpty(raw))
+            {
+                return "";
+            }
+            else
+            {
+                raw = raw.ToUpperInvariant();
+            }
+
+            var newNumber = new StringBuilder();
+            foreach (var c in raw)
+            {
+                if ("-0123456789".Contains(c))
+                {
+                    newNumber.Append(c);
+                }
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                    {
+                        newNumber.Append(result);
+                    }
+                }
+            }
+            return newNumber.ToString();
+        }
+
+        // Check if string contains character input, c
+        static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+
+        // Perform conversion a character to a number
+        // matching to a phone keypad.
+        static int? TranslateToNumber(char c)
+        {
+            if ("ABC".Contains(c))
+            {
+                return 2;
+            }
+            else if ("DEF".Contains(c))
+            {
+                return 3;
+            }
+            else if ("GHI".Contains(c))
+            {
+                return 4;
+            }
+            else if ("JKL".Contains(c))
+            {
+                return 5;
+            }
+            else if ("MNO".Contains(c))
+            {
+                return 6;
+            }
+            else if ("PQRS".Contains(c))
+            {
+                return 7;
+            }
+            else if ("TUV".Contains(c))
+            {
+                return 8;
+            }
+            else if ("WXYZ".Contains(c))
+            {
+                return 9;
+            }
+            return null;
+        }
+    }
+}
