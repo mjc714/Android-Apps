456e28bcce28357b5a3eae17a1b6c892f417f437
mjc714 <chin.matthew1212@gmail.com>
2017-11-24 00:08:52 -0500

    Phoneword Updates
    Added new activity for storing translation history of numbers.
    
    Added icon images.
    
    Added Translation History Button

--- Phoneword/Phoneword/MainActivity.cs
+++ Phoneword/Phoneword/MainActivity.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.App;
 using Android.Content;
 using Android.Widget;
 using Android.OS;
+using Core;
 
 namespace Phoneword
 {
-    [Activity(Label = "Phone Word", MainLauncher = true)]
+    [Activity(Label = "Phoneword", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        static readonly List<string> phoneNumbers = new List<string>();
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -20,23 +23,33 @@ namespace Phoneword
             EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
             TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
             Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button translationHistory = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
 
             // Add functionality to translate button.
+            string translatedNumber = string.Empty;
             translateButton.Click += (sender, e) =>
             {
-                // Convert input received in field to number.
-                string translatedNumber = Core.PhoneTranslator.ToNumber(phoneNumberText.Text);
+        // Translate user’s alphanumeric phone number to numeric
+        translatedNumber = PhoneTranslator.ToNumber(phoneNumberText.Text);
                 if (string.IsNullOrWhiteSpace(translatedNumber))
                 {
-                    translatedPhoneWord.Text = string.Empty;
+                    translatedPhoneWord.Text = "";
                 }
                 else
                 {
                     translatedPhoneWord.Text = translatedNumber;
+                    phoneNumbers.Add(translatedNumber);
+                    translationHistory.Enabled = true;
                 }
             };
 
-
+            // Add functionality to translation history button.
+            translationHistory.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+            };
         }
     }
 }
